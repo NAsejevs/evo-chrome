e3111f30fcaf142f38818bae59dbced702ca21b7
Nils Asejevs <nils.asejevs@gmail.com>
2020-02-08 20:37:34 +0200

    Adds test notifications

--- background.js
+++ background.js
@@ -1,8 +1,11 @@
+
 chrome.runtime.onInstalled.addListener(function() {
+	// Sets storage variable 'color' to beused in popup.js
 	chrome.storage.sync.set({color: '#3aa757'}, function() {
 		console.log("The color is green.");
 	});
 
+	// Adds a rule to show page_action only if we're on developer.chrome.com
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 		chrome.declarativeContent.onPageChanged.addRules([{
 			conditions: [
@@ -12,5 +15,28 @@ chrome.runtime.onInstalled.addListener(function() {
 			],
 			actions: [new chrome.declarativeContent.ShowPageAction()]
 		}]);
+	});
+
+	// Adds a message listener to listen for messages from page_action or any 
+	// injected script.
+	chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+		switch(request.type) {
+			case "notification": {
+				// Display test notification to user
+				chrome.notifications.create("betsOpen", {
+					type: "basic",
+					iconUrl: "images/evo_128.png",
+					title: "Title of bets open",
+					message: "Hello World!!"
+				}, () => {
+			
+				});
+				break;
+			}
+			default: {
+				sendResponse({error: "Invalid request type."});
+				break;
+			}
+		}
 	});
 });
--- popup.js
+++ popup.js
@@ -1,5 +1,13 @@
-let changeColor = document.getElementById('changeColor');
+let changeColor = document.getElementById('button');
 
+changeColor.onclick = () => {
+    // If the user clicks the button, let's show a test notification
+    chrome.runtime.sendMessage({
+        type: "notification"
+    });
+}
+
+// Using the set variable in background.js
 chrome.storage.sync.get('color', function(data) {
     changeColor.style.backgroundColor = data.color;
     changeColor.setAttribute('value', data.color);
